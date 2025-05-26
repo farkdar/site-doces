5f59542a04d837ed94497a2bf7cf00557784612c
Maicon Douglas <88475255+farkdar@users.noreply.github.com>
2025-05-26 15:31:20 -0300

    Update carregar-produtos.js

--- js/carregar-produtos.js
+++ js/carregar-produtos.js
@@ -2,12 +2,23 @@ async function carregarProdutos(categoria, containerId, arquivoJson) {
   try {
     const response = await fetch(arquivoJson);
     const data = await response.json();
-    const produtos = data.items; // agora acessa a chave "items"
+    const produtos = data.items;
 
     const container = document.getElementById(containerId);
     produtos.forEach(produto => {
       const card = document.createElement("div");
       card.className = "card-flip";
+
+      // Verifica se há lista de sabores
+      let saboresHtml = "";
+      if (produto.sabores && produto.sabores.length > 0) {
+        saboresHtml = `
+          <h3>Sabores</h3>
+          <ul class="lista-sabores-1">
+            ${produto.sabores.map(sabor => `<li>${sabor}</li>`).join("")}
+          </ul>`;
+      }
+
       card.innerHTML = `
         <div class="card-front">
           <img src="${produto.image}" alt="${produto.title}">
@@ -17,11 +28,13 @@ async function carregarProdutos(categoria, containerId, arquivoJson) {
         <div class="card-back">
           <h3>Composição</h3>
           <p>${produto.descricao}</p>
+          ${saboresHtml}
           <button class="botao-pedir" onclick="adicionarAoCarrinho('${produto.title}', ${produto.preco.replace(',', '.')})">
             Carrinho
           </button>
         </div>
       `;
+
       card.onclick = () => card.classList.toggle("flipped");
       container.appendChild(card);
     });
